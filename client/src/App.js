63eae5f57fbddf78d1e4d016653327a9a7f5443b
Lakshit Madaan <lakshitmadaan@gmail.com>
2021-08-20 14:38:02 +0530

    client updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar/Navbar'
+import Token from './components/Token';
+import User from './components/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/user' exact component={User}/>
+          <Route path='/token' exact component={Token}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
